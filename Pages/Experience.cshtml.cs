6c3c791c3c7cde776778a55b6169f9938999efe8
Andy Lam Yu <andyulam1999@gmail.com>
2023-04-12 22:02:35 -0700

    Add new experience and projects to each exp

--- Pages/Experience.cshtml.cs
+++ Pages/Experience.cshtml.cs
@@ -21,6 +21,7 @@ namespace ResumeWebsite.Pages
             Construction = 0,
             CustomerService = 1,
             InformationTech = 2,
+            Gift = 3
         }
 
         public JobType JobStatus { get; set; }
@@ -82,6 +83,9 @@ namespace ResumeWebsite.Pages
                 case IndustryType.Construction:
                     typeStr = "Construction";
                     break;
+                case IndustryType.Gift:
+                    typeStr = "Gift";
+                    break;
                 default:
                     typeStr = "Customer Service";
                     break;
@@ -109,6 +113,10 @@ namespace ResumeWebsite.Pages
                     "My responsibility was to troubleshoot raised bugs and implement a working fix to the issue\r\n        without introducing new bugs. Develop and complete a thorough unit test and test plans for\r\n        implemented changes, with documentation to help my fellow co-workers understand what the\r\n        changes are. While also, following the company’s programming standards. During projects, I\r\n        would communicate and contribute my design to the team and discuss a better solution to\r\n        implement."
                 )
             );
+            experiences[experiences.Count - 1].Projects.Add("Resolving reported bugs.");
+            experiences[experiences.Count - 1].Projects.Add("Adding new features to an internal tool.");
+            experiences[experiences.Count - 1].Projects.Add("Creating a new internal tool to monitor a projectors health.");
+            
             experiences.Add(
                 new Experience
                 (
@@ -121,6 +129,68 @@ namespace ResumeWebsite.Pages
                     "The project is a website that allows a service care business to manage its customers and\r\n        employees. The main feature enables the employee to be called in for work on short notice. The\r\n        website should display their availability and provide the customer details and needs.\r\n        My task has been adding missing features and functionality, such as creating the Rate Page for\r\n        the customer, which should display the rates details the hired staff will be getting per hour."
                 )
             );
+            experiences[experiences.Count - 1].Projects.Add("Adding missing or improving features to the Website pages");
+            experiences[experiences.Count - 1].Projects.Add("Adding missing or improving features to the accompany Mobile application");
+
+            experiences.Add(
+                new Experience
+                (
+                    JobType.PartTime,
+                    IndustryType.CustomerService,
+                    "TCU Buddies",
+                    "Murdoch University",
+                    new DateTime(2020, 02, 1),
+                    new DateTime(2020, 06, 1),
+                    "I acted as a guide and aided a group of foreign exchange student from Japan in adapting in Australia.\r\nMy duties was to communicate with the students and organize activities."
+                )
+            );
+            experiences[experiences.Count - 1].Projects.Add("Guiding the exchange students around Murdoch and Perth");
+            experiences[experiences.Count - 1].Projects.Add("Weekly meet ups");
+
+
+            experiences.Add(
+                new Experience
+                (
+                    JobType.Volunteer,
+                    IndustryType.CustomerService,
+                    "Open Day Welcomer",
+                    "Murdoch University",
+                    new DateTime(2019, 07, 28),
+                    new DateTime(2019, 07, 28),
+                    "Welcome visitors to Murdoch University and direct them to the location they are interested in, while giving them pamphlets."
+                )
+            );
+            experiences[experiences.Count - 1].Projects.Add("Welcoming Visitors");
+
+            experiences.Add(
+                new Experience
+                (
+                    JobType.Volunteer,
+                    IndustryType.Gift,
+                    "Open Day Gift Packager",
+                    "Murdoch University",
+                    new DateTime(2019, 07, 19),
+                    new DateTime(2019, 07, 19),
+                    "To help organise and package the Open Day Gift bags."
+                )
+            );
+            experiences[experiences.Count - 1].Projects.Add("Packaging gift bags");
+
+            experiences.Add(
+                new Experience
+                (
+                    JobType.WorkExperience,
+                    IndustryType.Construction,
+                    "Construction Trainee",
+                    "Ertech",
+                    new DateTime(2016, 01, 1),
+                    new DateTime(2017, 12, 1),
+                    "Training to gain skills that will be helpful to work as a construction worker."
+                )
+            );
+            experiences[experiences.Count - 1].Projects.Add("Laying PCV piping into the ground.");
+            experiences[experiences.Count - 1].Projects.Add("Laying concrete piping into the ground.");
+            experiences[experiences.Count - 1].Projects.Add("Learning to operate construction machine.");
         }
     }
 }
